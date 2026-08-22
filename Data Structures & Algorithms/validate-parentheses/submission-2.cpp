9a1374644c657a99a12f1bac26ff8c4e217612b2
Yahia Fawzy <125556483+YahiaHelal@users.noreply.github.com>
2026-08-22 19:19:34 +0300

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            }else {
+                if(s[i] == ')') {
+                    if(stk.size() > 0 && stk.top() == '(') {
+                        stk.pop();
+                    }else return false;
+                }else if(s[i] == ']') {
+                    if(stk.size() > 0 && stk.top() == '[') {
+                        stk.pop();
+                    }else return false;
+
+                }else if(s[i] == '}') {
+                    if(stk.size() > 0 && stk.top() == '{') {
+                        stk.pop();
+                    }else return false;
+                }
+            }
+        }
+        return stk.size() == 0;
+    }
+};
